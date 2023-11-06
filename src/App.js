da2f749a9e1fa32170e80a508a818f1fddc734cc
bsextion <bsextion@gmail.com>
2023-11-06 16:16:50 -0600

    Add axios config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import React, { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+      setMovies(response.data);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={Layout}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+function Layout() {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default Layout
